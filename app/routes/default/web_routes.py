e7997dcdc1a6f14872e8d0bb8b1b58f9987020e9
Salman Siddiqui <salmansiddiqui172002@gmail.com>
2025-04-24 15:08:29 +0300

    episode-2

--- app/routes/default/web_routes.py
+++ app/routes/default/web_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # initialize blueprint
 bp_default_web = Blueprint(
@@ -8,13 +8,28 @@ bp_default_web = Blueprint(
     url_prefix="/",
 )
 
+posts = [
+    {
+        "author": "Corey Schafer",
+        "titler": "Blog Post 1",
+        "content": "First post Content",
+        "date_posted": "April 20, 2018",
+    },
+    {
+        "author": "Corey Schafer",
+        "titler": "Blog Post 1",
+        "content": "First post Content",
+        "date_posted": "April 20, 2018",
+    },
+]
+
 
 @bp_default_web.route("/home")
 @bp_default_web.route("/")
 def home():
-    return "<h1>Hello, Flask!</h1>"
+    return render_template("home.html")
 
 
 @bp_default_web.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template("about.html")
