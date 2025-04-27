95d543e52ef45e688262b962322390e1e641ceac
Salman Siddiqui <salmansiddiqui172002@gmail.com>
2025-04-27 22:50:21 +0300

    episode-8

--- app/routes/post/web_routes.py
+++ app/routes/post/web_routes.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 from flask import Blueprint, abort, flash, redirect, render_template, request, url_for
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 from app.extensions import db
-from app.utils.forms import PostForm
+from app.utils.forms import PostForm, UpdatePostForm
 from app.utils.models import Post
 
 bp_post_web = Blueprint(
@@ -37,7 +37,7 @@ def new_post():
     db.session.add(post)
     db.session.commit()
 
-    flash(f"Post '<b>{form.title.data}</b>' created successfully.", category="success")
+    flash(f"Post <b>{form.title.data}</b> was created successfully.", category="success")
     return redirect(url_for("default_web.home"))
 
 
@@ -47,3 +47,84 @@ def post_page(post_id):
     if not post:
         abort(404)
     return render_template("single-post.html", title=post.title, post=post)
+
+
+@bp_post_web.route("/<int:post_id>/update", methods=["GET", "POST"])
+@login_required
+def update_post(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    if not post:
+        abort(404)
+    if not post.author == current_user:
+        abort(403)
+
+    form = UpdatePostForm()
+
+    # -- Handle GET Request
+    if request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+        return render_template("update-post-form.html", title="Edit Post", form=form, zip=zip)
+
+    # -- Handle POST Request
+    if not form.validate_on_submit():
+        flash(
+            "An error occurred when submitting the form. Please check the fields and try again.",
+            category="danger",
+        )
+        return render_template("update-post-form.html", title="Edit Post", form=form, zip=zip)
+
+    # update the post
+    post.title = form.title.data
+    post.content = form.content.data
+    db.session.commit()
+
+    flash(f"Post <b>{post.title}</b> was updated successfully.", category="success")
+    return redirect(url_for("post_web.post_page", post_id=post.id))
+
+    post = Post.query.filter_by(id=post_id).first()
+    if not post:
+        abort(404)
+    if not post.author == current_user:
+        abort(403)
+
+    form = UpdatePostForm()
+
+    # -- Handle GET Request
+    if request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+        return render_template("update-post-form.html", title="Edit Post", form=form, zip=zip)
+
+    # -- Handle POST Request
+    if not form.validate_on_submit():
+        flash(
+            "An error occurred when submitting the form. Please check the fields and try again.",
+            category="danger",
+        )
+        return render_template("update-post-form.html", title="Edit Post", form=form, zip=zip)
+
+    # update the post
+    post.title = form.title.data
+    post.content = form.content.data
+    db.session.commit()
+
+    flash(f"Post <b>{post.title}</b> was updated successfully.", category="success")
+    return redirect(url_for("post_web.post_page", post_id=post.id))
+
+
+@bp_post_web.route("/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    if not post:
+        abort(404)
+    if not post.author == current_user:
+        abort(403)
+
+    # delete the post
+    db.session.delete(post)
+    db.session.commit()
+
+    flash(f"Post <b>{post.title}</b> was deleted successfully.", category="success")
+    return redirect(url_for("default_web.home"))
--- app/utils/forms/__init__.py
+++ app/utils/forms/__init__.py
@@ -2,3 +2,4 @@ from .login_form import LoginForm  # noqa: F401
 from .registration_form import RegistrationForm  # noqa: F401
 from .account_update_form import AccountUpdateForm  # noqa: F401
 from .post_form import PostForm  # noqa: F401
+from .update_post_form import UpdatePostForm  # noqa: F401
